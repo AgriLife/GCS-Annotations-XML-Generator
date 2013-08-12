3673bdf7b7341d70534bf4fad955c8fd59ee2656
J. Aaron Eaton <aaron@channeleaton.com>
2013-08-12 12:12:09 -0500

    Added relationship methods to models

--- app/models/Site.php
+++ app/models/Site.php
@@ -4,4 +4,14 @@ class Site extends Eloquent {
     protected $guarded = array();
 
     public static $rules = array();
+
+    public function searches()
+    {
+    	return $this->belongsToMany('Search', 'searches_sites');
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo('User');
+    }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -7,4 +7,14 @@ class User extends ConfideUser {
 
 	use HasRole;
 
+	public function sites()
+	{
+		return $this->hasMany('Site');
+	}
+
+	public function searches()
+	{
+		return $this->hasMany('Search');
+	}
+
 }
