7ec886cc75334b0a4140084cf97ef9c779d09532
J. Aaron Eaton <aaron@channeleaton.com>
2013-08-23 10:22:24 -0500

    Installed Ardent

--- app/config/app.php
+++ app/config/app.php
@@ -145,6 +145,7 @@ return array(
 	'aliases' => array(
 
 		'App'             => 'Illuminate\Support\Facades\App',
+		'Ardent'					=> 'LaravelBook\Ardent\Ardent',
 		'Artisan'         => 'Illuminate\Support\Facades\Artisan',
 		'Auth'            => 'Illuminate\Support\Facades\Auth',
 		'Blade'           => 'Illuminate\Support\Facades\Blade',
